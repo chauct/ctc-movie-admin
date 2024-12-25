58c97e613070c34c9288f36bfddc55244a9e0590
chauct <caothichau222@gmail.com>
2024-12-25 20:20:42 +0700

    edit

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 const instance = axios.create({
-  baseURL: "https://movienew.cybersoft.edu.vn/",
+  baseURL: 'https://movienew.cybersoft.edu.vn/',
   headers: {
     TokenCybersoft:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNzIiLCJIZXRIYW5TdHJpbmciOiIxNC8wMi8yMDIzIiwiSGV0SGFuVGltZSI6IjE2NzYzMzI4MDAwMDAiLCJuYmYiOjE2NTAzODc2MDAsImV4cCI6MTY3NjQ4MDQwMH0.e3UrKdKqwFislz0cqribEEthuaW4HOuD4xwr1CTRQwg",
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA3OSIsIkhldEhhblN0cmluZyI6IjAzLzA5LzIwMjUiLCJIZXRIYW5UaW1lIjoiMTc1Njg1NzYwMDAwMCIsIm5iZiI6MTcyOTcwMjgwMCwiZXhwIjoxNzU3MDA1MjAwfQ.nPo29RkxTkE_C16RhJnxw90M3v3cu3Ur91a47F5epxA',
   },
 
   // timeout: 10000,
@@ -12,7 +12,7 @@ const instance = axios.create({
 instance.interceptors.request.use((config) => {
   config.headers = {
     ...config.headers,
-    Authorization: "Bearer " + localStorage.getItem("token"),
+    Authorization: 'Bearer ' + localStorage.getItem('token'),
   };
   return config;
 });
